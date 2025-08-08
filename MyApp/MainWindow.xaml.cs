4b8a1d7836fc7149d333215d711bafbfa94dc255
Oleksandr <aalexandr397@gmail.com>
2025-08-08 21:12:15 +0100

    UI added and tabs without  functions yet

--- MyApp/MainWindow.xaml.cs
+++ MyApp/MainWindow.xaml.cs
@@ -20,5 +20,21 @@ namespace MyApp
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            string objName = ((RadioButton)sender).Name;
+
+            StackPanel[] panels = { MainScreenPanel }; // UserListScreenpanel };
+            foreach (var panel in panels)
+                panel.Visibility = Visibility.Hidden; //Collapsed
+
+            switch (objName)
+            {
+                case "MainScreen": MainScreenPanel.Visibility = Visibility.Visible; break;
+                //case "UserListScreen": UserListScreenpanel.Visibility = Visibility.Visible; break;
+            }
+
+        }
     }
 }
