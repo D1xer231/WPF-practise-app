f6d7bf4476ce845ce6986c02869d04e583996a70
Oleksandr <aalexandr397@gmail.com>
2025-08-10 21:35:54 +0100

    register window +

--- MyApp/Models/Users.cs
+++ MyApp/Models/Users.cs
@@ -12,18 +12,15 @@ namespace MyApp.Models
         public string name { get; set; }
         public string email { get; set; }
         public string password { get; set; }
-        public DateTime created_at { get; set; }
         public string fav_meth { get; set; }
 
         public Users() {}
 
-        public Users(int id, string name, string email, string password, DateTime created_at, string fav_meth)
+        public Users(string name, string email, string password, string fav_meth)
         {
-            this.id = id;
             this.name = name;
             this.email = email;
             this.password = password;
-            this.created_at = created_at;
             this.fav_meth = fav_meth;
         }
     }
---
+++ MyApp/RegisterWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using MyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace MyApp
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        private AppDbContext _db;
+
+        public RegisterWindow()
+        {
+            InitializeComponent();
+            _db = new AppDbContext();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = UsernameField.Text.Trim();
+            string email = EmailField.Text.Trim();
+            string password = PasswordField.Password.Trim();
+            string favMeth = FavMethField.Text.Trim();
+            if (name.Equals("") || !email.Contains("@") || password.Length < 4 || favMeth.Equals(""))
+            {
+                MessageBox.Show("All fields are required.");
+                return;
+            }
+
+            Users AuthUser = _db.Users.Where(el => el.email == email).FirstOrDefault();
+            if (AuthUser != null)
+            {
+                MessageBox.Show("User with this email already exists.");
+                return;
+            }
+
+            Users user = new Users(name, email, Hash(password), favMeth);
+            _db.Users.Add(user);
+            _db.SaveChanges();
+
+            UsernameField.Text = "";
+            EmailField.Text = "";
+            PasswordField.Password = "";
+            FavMethField.Text = "";
+            RegisterButton.Content = "Registered!";
+            MessageBox.Show("Registration successful!");
+        }
+
+        private string Hash(string input)
+        {
+            byte[] temp = Encoding.UTF8.GetBytes(input);
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(temp);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            //AuthWindow window = new AuthWindow();
+            //window.Show();
+            Close();
+        }
+    }
+}
